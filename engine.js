3054358cbc9b1f09360e3f19da9062ac81804fd7
Charles Hughes <c.hughes87@gmail.com>
2013-08-12 20:22:31 -0500

    working on collision detection.

--- engine.js
+++ engine.js
@@ -9,15 +9,16 @@ var downx;
 var downy;
 var upx;
 var upy;
-var particles = [];
+var old_world = [];
+var new_world = [];
 
 var particle = (function (x, y, vx, vy) {
   return {
-    getX: function (){
-      return x;
+    getPosition: function (){
+      return {x: x, y: y};
     },
-    getY: function (){
-      return y;
+    getVelocity: function (){
+      return {x: vx, y: vy};
     },
     distanceFrom: function (xc, yc){
       var dx = x - xc;
@@ -25,49 +26,74 @@ var particle = (function (x, y, vx, vy) {
       var dist = Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
       var xComp = dist*dist/dx;
       var yComp = dist*dist/dy;
-      return {x: xComp, y: yComp};
+      return {total: dist, x: xComp, y: yComp};
     },
-    iterate: function (){
+    calcGrav: function (x2, y2)
+    {
+      dist = this.distanceFrom(x2,y2);
       var forceX = 0;
       var forceY = 0;
+      if(dist.x != 0){
+        sign = dist.x/Math.abs(dist.x);
+        forceX += sign/Math.pow(dist.x,2);
+      }
+      if(dist.y != 0){
+        sign = dist.y/Math.abs(dist.y);
+        forceY += sign/Math.pow(dist.y,2);
+      }
+      return {x: forceX, y: forceY};
+    },
+    detectCollision: function (p)
+    {
+      vel = p.getVelocity();
+      pos = p.getPosition();
+      if(this.distanceFrom(pos.x,pos.y).total <= 20)
+        return 1;
+      else
+        return 0;
+    },
+    iterate: function (){
+      var force;
       var dist;
       var sign = 0;
 
-      circle(x, y, 5);
-      for(var j = 0; j < particles.length; j++){
-        if(particles[j] === this)
-          continue;
-        dist = particles[j].distanceFrom(x,y);
-        if(dist.dx != 0){
-          sign = dist.x/Math.abs(dist.x);
-          forceX += sign/Math.pow(dist.x,2);
-        }
-        if(dist.dy != 0){
-          sign = dist.y/Math.abs(dist.y);
-          forceY += sign/Math.pow(dist.y,2);
+      circle(x, y, 10);
+
+      for(var j = 0; j < old_world.length; j++){
+        if(old_world[j] !== this){
+          force = old_world[j].calcGrav(x,y);
+          vx += force.x*1000;
+          vy += force.y*1000;
+          if(this.detectCollision(old_world[j])){
+            document.getElementById('debug1').innerHTML = "COLLISION!";
+            vx = old_world[j].getVelocity.x;
+            vy = old_world[j].getVelocity.y;
+            // vx += -force.x*1000;
+            // vy += -force.y*1000;
+          }
         }
       }
-      vx += forceX*1000;
-      vy += forceY*1000;
+
+
       if (x + vx > WIDTH || x + vx < 0)
         vx = -vx;
       if (y + vy > HEIGHT || y + vy < 0)
         vy = -vy;
-      line(x,y,x+forceX*100,y+forceY*100,'#499df5')
-      line(x,y,x+vx*10,y+vy*10,'#ff0000');
+
+      line(x,y,x+vx,y+vy,'#ff0000');
       y += vy;
       x += vx;
-      document.getElementById('debug1').innerHTML = "forceX: "+forceX;
-      document.getElementById('debug2').innerHTML = "forceY:"+forceY;
-      document.getElementById('debug3').innerHTML = "vx: "+vx;
-      document.getElementById('debug4').innerHTML = "vy: "+vy;
+      // document.getElementById('debug1').innerHTML = "forceX: "+force.x;
+      // document.getElementById('debug2').innerHTML = "forceY:"+force.y;
+      // document.getElementById('debug3').innerHTML = "vx: "+vx;
+      // document.getElementById('debug4').innerHTML = "vy: "+vy;
     }
   };
 });
 
 function addParticle(x,y,dx,dy)
 {
-  particles.push(particle(x,y,dx,dy));//{'x':x,'y':y,'dx':dx,'dy':dy});
+  old_world.push(particle(x,y,dx,dy));//{'x':x,'y':y,'dx':dx,'dy':dy});
 }
 
 // function init_mouse() {
@@ -95,9 +121,15 @@ function onMouseUp(evt)
   upy = evt.pageY;
 }
 
+function onKeyPress(evt)
+{
+  draw();
+}
+
 document.onmousedown = onMouseDown;
 document.onmouseup = onMouseUp;
 document.onclick = onMouseClick;
+document.onkeypress = onKeyPress;
 
 function circle(x,y,r) {
   ctx.beginPath();
@@ -124,14 +156,15 @@ function init() {
   WIDTH = cnvs.width;
   HEIGHT = cnvs.height;
   ctx.font="12px Arial";
-  return setInterval(draw, 100);
+  // return setInterval(draw, 100);
 }
 
 function draw() {
   clear();
-  for(var i = 0; i < particles.length; i++)
+  new_world = old_world;
+  for(var i = 0; i < old_world.length; i++)
   {
-    particles[i].iterate();
+    new_world[i].iterate();
   }
 }
 
